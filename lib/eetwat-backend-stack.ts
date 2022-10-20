07ee51e4b1fc3c9b02e9e8f19c06507691f3028e
Timo Benkhard <tcbenkhard@gmail.com>
2022-10-20 13:30:34 +0200

    Pipeline

--- lib/eetwat-backend-stack.ts
+++ lib/eetwat-backend-stack.ts
@@ -1,16 +1,54 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Construct} from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as api from 'aws-cdk-lib/aws-apigateway';
+import {LambdaIntegration} from 'aws-cdk-lib/aws-apigateway';
+import * as dynamo from 'aws-cdk-lib/aws-dynamodb';
+import {AttributeType} from 'aws-cdk-lib/aws-dynamodb';
+import {aws_ssm} from "aws-cdk-lib";
+
+interface EetwatBackendProps extends cdk.StackProps {
+  environment: 'tst'|'prd'
+  serviceName: string
+}
 
 export class EetwatBackendStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: EetwatBackendProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const mealsTable = new dynamo.Table(this, `${props.serviceName}-${props.environment}-table`, {
+      tableName: `${props.serviceName}-${props.environment}-table`,
+      partitionKey: {
+        type: AttributeType.STRING,
+        name: 'id'
+      }
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EetwatBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const environment = {
+      MEALS_TABLE_NAME: mealsTable.tableName
+    }
+
+    const listMealsHandler = new lambda.NodejsFunction(this, `${props.serviceName}-${props.environment}-list-meals`, {
+      handler: 'handler',
+      entry: 'src/list-meals-handler.ts',
+      functionName: `${props.serviceName}-${props.environment}-list-meals`,
+      environment
+    })
+    mealsTable.grantReadData(listMealsHandler);
+
+    const gateway = new api.RestApi(this, `${props.serviceName}-${props.environment}`, {
+      restApiName: `${props.serviceName}-${props.environment}`,
+      defaultCorsPreflightOptions: {
+        allowOrigins: ['*']
+      }
+    });
+
+    const gatewayMeals = gateway.root.addResource('meals');
+    gatewayMeals.addMethod('GET', new LambdaIntegration(listMealsHandler));
+
+    new aws_ssm.StringParameter(this, `/${props.serviceName}/${props.environment}/hostname`, {
+      parameterName: `/${props.serviceName}/${props.environment}/hostname`,
+      stringValue: gateway.url
+    });
   }
 }
--- src/schema/meal.ts
+++ src/schema/meal.ts
@@ -3,6 +3,7 @@ import S from 'jsonschema-definer';
 const MealSchema = S.shape({
     id: S.string(),
     name: S.string(),
+    durationInMinutes: S.number(),
     tags: S.list(S.string()),
 });
 
